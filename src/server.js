987ffbec98ab481355b1deb51d0e5eaedb4b1bcd
Vinicius Faria de Souza <vinicius_ufop@yahoo.com.br>
2018-10-31 23:19:57 -0300

    teste de configuração do heroku na port 5000

--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const PORT = process.env.PORT || 5000;
 
 const app = express();
-
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/receitas-front'));
+app.use(express.static(__dirname + '/../dist/receitas-front'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/receitas-front/index.html'));
+  res.sendFile(path.join(__dirname+'/../dist/receitas-front/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(PORT);
